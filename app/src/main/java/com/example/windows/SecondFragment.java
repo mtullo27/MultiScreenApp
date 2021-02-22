a794c6d34ffaeffb6b8be871792db450e1a0f530
mtullo27 <mtullo1@binghamton.edu>
2021-02-22 11:16:49 -0500

    login started

--- app/src/main/java/com/example/windows/SecondFragment.java
+++ app/src/main/java/com/example/windows/SecondFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.windows.ui.login.Login;
 
 public class SecondFragment extends Fragment {
 
